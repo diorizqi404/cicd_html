26118875eabe7ace00a0fcd02739e5b9bfa53d30
ini nama <rizqiii.dio@gmail.com>
2024-12-26 12:32:13 +0700

    change

--- scripts/configure_permissions.sh
+++ scripts/configure_permissions.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Set folder Laravel
-APP_DIR="/var/www/html"
-
 # Set permission
-chown -R apache:apache $APP_DIR
-chmod -R 775 $APP_DIR/storage $APP_DIR/bootstrap/cache
+sudo chown -R apache:apache /var/www/html/storage /var/www/html/bootstrap/cache
+sudo chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
