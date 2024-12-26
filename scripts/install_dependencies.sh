aa4a9eaf23f783b950fdea15250cbf9b38d887ea
ini nama <rizqiii.dio@gmail.com>
2024-12-26 12:34:14 +0700

    change

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@
 dnf update -y
 
 # Instal PHP 8.2 dan ekstensi yang dibutuhkan
-dnf install -y php php-cli php-common php-mbstring php-xml php-bcmath php-curl php-zip unzip git
+dnf install -y php php-cli php-common php-mbstring php-xml php-bcmath php-mysqlnd php-curl php-zip unzip git
 
 # Instal Composer
 curl -sS https://getcomposer.org/installer | php
